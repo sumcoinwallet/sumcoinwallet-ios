305ce78686e25360c2541617464ea89f89857c6d
kcw-grunt <mrkerrywashington@icloud.com>
2021-04-05 19:18:00 +0100

    added optionals for CardWalletDetails

--- loafwallet/CardWalletDetails.swift
+++ loafwallet/CardWalletDetails.swift
@@ -10,13 +10,13 @@ import Foundation
 
 struct CardWalletDetails: Decodable {
     
-    var balance: Float
-    var ltcAddress: String
-    var createdAt: String
-    var updatedAt: String
-    var availableBalance: Float
-    var withdrawableBalance: Float
-    var spendableBalance: Float
+    var balance: Float?
+    var ltcAddress: String?
+    var createdAt: String?
+    var updatedAt: String?
+    var availableBalance: Float?
+    var withdrawableBalance: Float?
+    var spendableBalance: Float?
     
     enum CodingKeys: String, CodingKey {
         case balance = "balance"
@@ -32,15 +32,14 @@ struct CardWalletDetails: Decodable {
         
         let container = try decoder.container(keyedBy: CodingKeys.self)
         
-        balance = try! container.decode(Float.self, forKey: .balance)
-        ltcAddress = try! container.decode(String.self, forKey: .ltcAddress)
-        createdAt = try! container.decode(String.self, forKey: .createdAt)
-        updatedAt = try! container.decode(String.self, forKey: .updatedAt)
-        availableBalance = try! container.decode(Float.self, forKey: .availableBalance)
-        withdrawableBalance = try! container.decode(Float.self, forKey: .withdrawableBalance)
-        spendableBalance = try! container.decode(Float.self, forKey: .spendableBalance)
-    }
-    
+        balance = try? container.decode(Float.self, forKey: .balance)
+        ltcAddress = try? container.decode(String.self, forKey: .ltcAddress)
+        createdAt = try? container.decode(String.self, forKey: .createdAt)
+        updatedAt = try? container.decode(String.self, forKey: .updatedAt)
+        availableBalance = try? container.decode(Float.self, forKey: .availableBalance)
+        withdrawableBalance = try? container.decode(Float.self, forKey: .withdrawableBalance)
+        spendableBalance = try? container.decode(Float.self, forKey: .spendableBalance)
+    } 
 }
 
 
